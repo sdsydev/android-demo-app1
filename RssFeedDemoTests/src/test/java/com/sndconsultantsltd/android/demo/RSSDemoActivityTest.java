fc46d7e0905a29b1ee77b4e3ec38e3344466e5cd
sallochystu <sallochystu@gmail.com>
2015-06-05 11:01:25 +0100

    added unit test project with basic working test

---
+++ RssFeedDemoTests/src/test/java/com/sndconsultantsltd/android/demo/RSSDemoActivityTest.java
@@ -0,0 +1,17 @@
+package com.sndconsultantsltd.android.demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class RSSDemoActivityTest {
+	
+	@Test
+	public void shouldPass(){
+		assertTrue(true);
+	}
+
+}
