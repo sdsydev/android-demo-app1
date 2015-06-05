0ebc41e7b0a7820c8468b961650708e57efa48bf
sallochystu <sallochystu@gmail.com>
2015-06-05 11:23:32 +0100

    added robolectric /assertj test to check for nulls

--- RssFeedDemoTests/src/test/java/com/sndconsultantsltd/android/demo/RSSDemoActivityTest.java
+++ RssFeedDemoTests/src/test/java/com/sndconsultantsltd/android/demo/RSSDemoActivityTest.java
@@ -1,17 +1,42 @@
 package com.sndconsultantsltd.android.demo;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import android.os.Build;
+
+import com.example.rssfeeddemo.RSSDemoActivity;
 
 @RunWith(RobolectricTestRunner.class)
+@Config(manifest = "../RssFeedDemo/AndroidManifest.xml", emulateSdk = Build.VERSION_CODES.JELLY_BEAN_MR2)
 public class RSSDemoActivityTest {
+
+	private RSSDemoActivity rssDemoAct;
+
+	@Test
+	public void shouldPass() {
+		assertTrue(true);
+	}
+
+	@Before
+	public void setup() {
+
+		rssDemoAct = Robolectric.buildActivity(RSSDemoActivity.class).create()
+				.get();
+
+	}
 	
 	@Test
-	public void shouldPass(){
-		assertTrue(true);
+	public void allViewsPresent(){
+		
+		assertThat(rssDemoAct).isNotNull();
 	}
 
 }
